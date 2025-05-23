3809bbd2f383aebc862cebb9b529c426a69a8793
Abjorny <123demonpro123@gmail.com>
2025-05-23 17:33:17 +1000

    update

--- main/wss/consumers.py
+++ main/wss/consumers.py
@@ -36,7 +36,7 @@ sensor_right_one =  Sensor(
 )
 
 sensor_center_one =  Sensor(
-    np.array([[310, 265], [645, 270], [655, 480], [310, 480]]), 
+    np.array([[340, 265], [645, 270], [640, 480], [340, 480]]), 
     (0, 0, 255) 
 )
 
