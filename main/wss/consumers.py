869e4905123fae964557b570d7cb9a20e7d05ec4
Abjorny <123demonpro123@gmail.com>
2025-05-23 12:08:27 +1000

    update

--- main/wss/consumers.py
+++ main/wss/consumers.py
@@ -3,28 +3,41 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
 import json
 import cv2
-import asyncio
 import base64
-from channels.layers import get_channel_layer
+
 task_started = False
 
 async def send_periodic_messages():
     channel_layer = get_channel_layer()
-    cap = cv2.VideoCapture(0) 
-    if not cap.isOpened():
+    cap0 = cv2.VideoCapture(0)
+    cap2 = cv2.VideoCapture(2)
+    
+    if not cap0.isOpened() or not cap2.isOpened():
+        print("One of the cameras did not open")
+        if cap0.isOpened():
+            cap0.release()
+        if cap2.isOpened():
+            cap2.release()
         return
 
     try:
         while True:
-            ret, frame = cap.read()
+            ret0, frame0 = cap0.read()
+            ret2, frame2 = cap2.read()
 
-            if not ret:
+            if not ret0 or not ret2:
+                await asyncio.sleep(0.01)
                 continue
             
-
-            _, buffer = cv2.imencode('.jpg', frame)
-
+            if frame0.shape != frame2.shape:
+                frame2 = cv2.resize(frame2, (frame0.shape[1], frame0.shape[0]))
+            
+            # Объединяем два кадра по горизонтали
+            combined_frame = cv2.hconcat([frame0, frame2])
+            
+            _, buffer = cv2.imencode('.jpg', combined_frame)
             image_data = base64.b64encode(buffer).decode('utf-8')
+
             await channel_layer.group_send(
                 "broadcast_group",
                 {
@@ -35,7 +48,8 @@ async def send_periodic_messages():
 
             await asyncio.sleep(0.01)  
     finally:
-        cap.release()
+        cap0.release()
+        cap2.release()
 
 class MyConsumer(AsyncWebsocketConsumer):
     async def connect(self):
