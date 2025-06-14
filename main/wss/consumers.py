f65b79cbc5927790eccfd810eed699bb1726d72a
Abjorny <123demonpro123@gmail.com>
2025-06-14 17:12:46 +1000

    update

--- main/wss/consumers.py
+++ main/wss/consumers.py
@@ -344,10 +344,10 @@ async def read_data():
     roi3 = await sensor_center_left.get_roi(frame).roi_frame
     roi4 = await sensor_center_right.get_roi(frame).roi_frame
 
-    value_center_one = predict_image_class(roi1, model_data)
-    value_center_two = predict_image_class(roi2, model_data)
-    value_left = predict_image_class(roi3, model_data)
-    value_right = predict_image_class(roi4, model_data)
+    value_center_one = await predict_image_class(roi1, model_data)
+    value_center_two = await predict_image_class(roi2, model_data)
+    value_left =await  predict_image_class(roi3, model_data)
+    value_right = await predict_image_class(roi4, model_data)
 
     # value_center_one, isTwo = sensor_center_one.readObject(copyFrame, frame)
 
