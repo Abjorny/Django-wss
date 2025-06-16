4d532d34dbb5aabf30f867fd3d3898fbf4f09270
Abjorny <123demonpro123@gmail.com>
2025-06-16 17:44:42 +1000

    update

--- main/wss/VirtualEye/FrameUtilis.py
+++ main/wss/VirtualEye/FrameUtilis.py
@@ -20,7 +20,7 @@ class FrameUtilis:
         for sensor in sensors:
             if sensor.show:
                 cv2.polylines(frame, [sensor.mass_display], isClosed=True, color=sensor.color, thickness=2)
-                cv2.polylines(frame, [sensor.mass_display_check], isClosed=True, color=sensor.color, thickness=2)
+                # cv2.polylines(frame, [sensor.mass_display_check], isClosed=True, color=sensor.color, thickness=2)
                 # cv2.polylines(frame, [sensor.massThree], isClosed=True, color=sensor.color, thickness=2)
                 # cv2.polylines(frame, [sensor.massCompress], isClosed=True, color=(255,0,255), thickness=2)
                 # cv2.polylines(frame, [sensor.massForCheck], isClosed=True, color=sensor.color, thickness=2)
