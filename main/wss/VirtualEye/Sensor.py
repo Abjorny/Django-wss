1e7fbf9a24f4c6c3bcd547d05d703c8b0f813fcb
Abjorny <123demonpro123@gmail.com>
2025-06-17 18:26:11 +1000

    update

--- main/wss/VirtualEye/Sensor.py
+++ main/wss/VirtualEye/Sensor.py
@@ -374,7 +374,7 @@ class RedSensor(Sensor):
         roi: Roi = self.get_roi(frame)
         result: Result = self.get_red(roi, frame_3d)
         if ( result.w * result.h  > 0):
-            print(roi.w * roi.h / result.w * result.h * 100 )
+            print(roi.w * roi.h / result.area * 100 )
         isBorder = False
         if result.w * result.h > 0 and roi.w * roi.h / result.w * result.h * 100 > 90:
             isBorder = True
