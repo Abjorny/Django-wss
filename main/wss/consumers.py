f49c00fb3289e45ab8111bb48f92256670e6ac19
Abjorny <123demonpro123@gmail.com>
2025-06-20 14:10:32 +1000

    update

--- main/wss/consumers.py
+++ main/wss/consumers.py
@@ -382,16 +382,16 @@ async def read_data():
         value_right, confidence_right = predict_image_classpredict(roi4)
 
 
-    if value_center_one in [31, 32, 33, 34, 23, 24, 41]:
+    if value_center_one in [31, 32, 33, 34, 21, 22, 23, 24, 41]:
         value_center_one, confidence_one =  sensor_center_one.readObject(copyFrame, frame, value_center_one)
     
-    if value_center_two in [31, 32, 33, 34, 23, 24, 41]:
+    if value_center_two in [31, 32, 33, 34, 21, 22,  23, 24, 41]:
         value_center_two, confidence_two = sensor_center_two.readObject(copyFrame, frame, value_center_two)
     
-    if value_left in [31, 32, 33, 34, 23, 24, 41]:
+    if value_left in [31, 32, 33, 34, 21, 22, 23, 24, 41]:
         value_left, confidence_left = sensor_center_left.readObject(copyFrame, frame, value_left)
     
-    if value_right in [31, 32, 33, 34, 23, 24, 41]:
+    if value_right in [31, 32, 33, 34, 21, 22, 23, 24, 41]:
         value_right, confidence_right = sensor_center_right.readObject(copyFrame, frame, value_right)
 
 
