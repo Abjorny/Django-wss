ddc97e39ab33f018e5a8088bcefcdbeff70248bd
Abjorny <123demonpro123@gmail.com>
2025-08-11 12:57:53 +1000

    update

--- main/wss/utilis.py
+++ main/wss/utilis.py
@@ -24,13 +24,13 @@ def returnAngleItem(data, sensorData, frame):
     quater = 0
 
     if x_center_item > x_center_frame:
-        if y_center_item < y_center_frame:
+        if y_center_item > y_center_frame:
             quater = 1
         else:
-            quater = 2
-    else:
-        if y_center_item < y_center_frame:
             quater = 4
+    else:
+        if y_center_item > y_center_frame:
+            quater = 2
         else:
             quater = 3
     
