ce9bad8ec57b8c9ffb52bd5aa900e42fdd63b4f2
Abjorny <123demonpro123@gmail.com>
2025-08-11 14:17:58 +1000

    update

--- main/wss/utilis.py
+++ main/wss/utilis.py
@@ -57,7 +57,7 @@ def returnAngleItem(data, sensorData, frame):
         if delta_y > delta_x:
             angle = 180 + math.degrees(math.atan(delta_x / delta_y))
         else:
-            angle = 225 - math.degrees(math.atan(delta_y / delta_x))
+            angle = 270 - math.degrees(math.atan(delta_y / delta_x))
 
     elif quater == 4:
         if delta_y > delta_x:
