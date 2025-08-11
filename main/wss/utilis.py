c9600c167151e784af786fd5f0ba40a3bcfb89a1
Abjorny <123demonpro123@gmail.com>
2025-08-11 13:02:54 +1000

    update

--- main/wss/utilis.py
+++ main/wss/utilis.py
@@ -23,16 +23,18 @@ def returnAngleItem(data, sensorData, frame):
 
     quater = 0
 
-    if x_center_item > x_center_frame:
-        if y_center_item > y_center_frame:
-            quater = 1
-        else:
-            quater = 4
-    else:
-        if y_center_item < y_center_frame:
+    delta_x = x_center_frame - x_center_item
+    delta_y = y_center_frame - y_center_item
+    if delta_x > 0:
+        if delta_y > 0:
             quater = 2
         else:
             quater = 3
+    else:
+        if delta_y > 0:
+            quater = 1
+        else:
+            quater = 4
     
     cv2.line(frame, (x_center_frame, y_center_frame), (x_center_item, y_center_item), (0, 255, 255), 2)
     return quater
