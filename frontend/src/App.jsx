5594b3b4594a8352cb204a6a496917e7474447ac
Abjorny <123demonpro123@gmail.com>
2025-08-12 12:33:24 +1000

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,40 @@
-import Camera from "./components/Camera/Camera"
-import { useState } from "react"
+import Camera from "./components/Camera/Camera";
+import Stereo from "./components/Stereo/Stereo";
+import { useState } from "react";
 
 function App() {
   const [page, setPage] = useState("camera");
 
   const pages = {
-    "camera" : <Camera />
-  }
+    camera: {
+      value: <Camera />,
+      text: "Камера",
+    },
+    stereo: {
+      value: <Stereo />,
+      text: "Стерео",
+    },
+  };
+
   return (
     <>
-      <div class="container-navigator">
-        <div class="navigrator active" data-value="container-camera">Камера</div>
+      <div className="container-navigator">
+        {Object.entries(pages).map(([key, { text }]) => (
+          <div
+            key={key}
+            className={`navigator ${page === key ? "active" : ""}`}
+            onClick={() => setPage(key)}
+          >
+            {text}
+          </div>
+        ))}
       </div>
 
       <div id="container-camera">
-        {pages[page]}
+        {pages[page].value}
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Stereo/Stereo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Stereo.css";
+
+const Stereo = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Stereo;
