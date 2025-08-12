e6e440bc4f42c34734e0b9eadef668d4d819b0d2
Abjorny <123demonpro123@gmail.com>
2025-08-12 13:24:26 +1000

    update

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,3 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io("ws://${window.location.hostname}:8000/ws/api/get_image");
+export const socket = io(`ws://${window.location.hostname}:8000`, {
+  path: '/ws/api/get_image'
+});
