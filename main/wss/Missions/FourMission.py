08b55e12eff0acb7a1781f93640694841b16f2a0
Abjorny <123demonpro123@gmail.com>
2025-09-04 17:07:24 +1000

    update

--- main/wss/Missions/FourMission.py
+++ main/wss/Missions/FourMission.py
@@ -69,7 +69,7 @@ async def goToBlack(frame, sensor_find):
             await printLog("Робот сдох")
         e = camera.FIXED_WIDTH // 2  - (x1 + w1 // 2)
 
-        U = utilis.u_colcultor(e, EOLD)
+        U = utilis.u_colcultor(e, EOLD, 0.5)
 
         MA = 20 - U
         MB = 20 + U
