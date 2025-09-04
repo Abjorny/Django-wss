4bb43f322c0d89cec35be222d84aca3ed1551079
Abjorny <123demonpro123@gmail.com>
2025-09-04 17:16:58 +1000

    update

--- main/wss/Missions/utilis.py
+++ main/wss/Missions/utilis.py
@@ -1,10 +1,10 @@
 def constrain(MA, MB):
     MA = int(MA)
     MB = int(MB)
-    if MA > 15: MA = 15
-    if MB > 20: MB = 20
+    if MA > 30: MA = 30
+    if MB > 30: MB = 30
 
-    if MA < -15: MA = -15
+    if MA < -20: MA = -20
     if MB < -20: MB = -20
 
     return MA, MB
